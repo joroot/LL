5b23ffbf2d2826944cada6ce1145c2a4a6aaf3e5
JokerPlus0615 <38981120+joroot@users.noreply.github.com>
2022-08-24 23:22:20 +0800

    LL I2C

---
+++ I2C_TwoBoards_MasterTx_SlaveRx_Init/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'I2C_TwoBoards_MasterTx_SlaveRx_Init' 
+ * Target:  'I2C_TwoBoards_MasterTx_SlaveRx_Init' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
